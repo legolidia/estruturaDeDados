771963b318f052281b2becad8a5b1f5ca6a81884
legolidia <lidiacarolinaandrade@gmail.com>
2023-08-13 20:14:14 -0300

    exercicio 1

---
+++ exercicio01.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class exercicio01 {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    int M = 0; // M é a quantidade de tanques cilíndricos
+
+    System.out.println("Digite a quantidade de tanques cilíndricos: ");
+    M = s.nextInt();
+
+    float h = 0, r = 0, volume = 0, volumeTotal = 0;
+
+    System.out.println("Digite a altura do tanque: ");
+    h = s.nextFloat();
+
+    System.out.println("Digite o raio do tanque: ");
+    r = s.nextFloat();
+
+    volume = (float) (Math.PI * Math.pow(r, 2) * h);
+    volumeTotal = volume * M;
+
+    float barris = volumeTotal / 200;
+
+    System.out.println("Sao necessarios" + Math.ceil(barris) + "barris de petroleo de 200L para encher os tanques");
+
+  }
+}
